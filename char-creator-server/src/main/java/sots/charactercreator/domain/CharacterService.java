9c434f496e4fcf4a1c169568b87f9d44231792aa
Henry <hthi1801@gmail.com>
2023-01-06 11:38:38 -0600

    finished and tested basic character domain layer

--- char-creator-server/src/main/java/sots/charactercreator/domain/CharacterService.java
+++ char-creator-server/src/main/java/sots/charactercreator/domain/CharacterService.java
@@ -26,4 +26,68 @@ public class CharacterService {
     public List<Character> findCharactersByUser(int userId) {
         return repository.findCharactersByUser(userId);
     }
+
+    public Result<Character> addCharacter(Character character) {
+        Result<Character> result = validate(character);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        repository.addCharacter(character);
+
+        return result;
+    }
+
+    public Result<Character> updateCharacter(Character character) {
+        Result<Character> result = validate(character);
+        if (character.getCharacterId() < 0) {
+            result.addMessage("Invalid Character ID", ResultType.INVALID);
+            return result;
+        }
+
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (!repository.updateCharacter(character)) {
+            result.addMessage("Character not found", ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public boolean deleteCharacter(int characterId) {
+        return repository.deleteCharacter(characterId);
+    }
+
+    private Result<Character> validate(Character character) {
+        Result<Character> result = new Result<>();
+        result.setPayload(character);
+        if (character == null) {
+            result.addMessage("Character cannot be null", ResultType.INVALID);
+            return result;
+        }
+
+        if (character.getSpeciesId() < 1) {
+            result.addMessage("Invalid Species ID", ResultType.INVALID);
+        }
+
+        if (character.getClassId() < 1) {
+            result.addMessage("Invalid Class ID", ResultType.INVALID);
+        }
+
+        if (character.getBackgroundId() < 1) {
+            result.addMessage("Invalid Background ID", ResultType.INVALID);
+        }
+
+        if (character.getAlignmentId() < 1) {
+            result.addMessage("Invalid Alignment ID", ResultType.INVALID);
+        }
+
+        if (character.getAppUserId() < 1) {
+            result.addMessage("Invalid User ID", ResultType.INVALID);
+        }
+
+        return result;
+    }
 }
--- char-creator-server/src/test/java/sots/charactercreator/domain/CharacterServiceTest.java
+++ char-creator-server/src/test/java/sots/charactercreator/domain/CharacterServiceTest.java
@@ -15,6 +15,120 @@ class CharacterServiceTest {
     @Test
     void shouldFindAll() {
         List<Character> all = service.findAllCharacters();
-        assertEquals(3, all.size());
+        assertEquals(all.get(1).getCharacterName(), "Jobby");
+    }
+
+    @Test
+    void shouldFindByUser() {
+        List<Character> allUser = service.findCharactersByUser(2);
+        assertEquals(allUser.get(1).getCharacterName(), "Jobby");
+    }
+
+    @Test
+    void shouldFindById() {
+        Character c = service.findCharacterById(2);
+        assertEquals("Jobby", c.getCharacterName());
+    }
+
+    @Test
+    void shouldAddValid() {
+        Character c = new Character();
+        c.setCharacterName("Dobby");
+        c.setSpeciesId(4);
+        c.setClassId(4);
+        c.setBackgroundId(4);
+        c.setAlignmentId(4);
+        c.setAppUserId(3);
+        int prevSize = service.findAllCharacters().size();
+        assertTrue(service.addCharacter(c).isSuccess());
+        assertEquals(prevSize + 1, service.findAllCharacters().size());
+    }
+
+    @Test
+    void shouldNotAddNull() {
+        Character c = new Character();
+        assertFalse(service.addCharacter(c).isSuccess());
+    }
+
+    @Test
+    void shouldNotAddMissingSpecies() {
+        Character c = new Character();
+        c.setCharacterName("Blobby");
+        c.setClassId(2);
+        c.setBackgroundId(3);
+        c.setAlignmentId(4);
+        c.setAppUserId(3);
+        assertFalse(service.addCharacter(c).isSuccess());
+    }
+
+    @Test
+    void shouldNotAddMissingClass() {
+        Character c = new Character();
+        c.setCharacterName("Blobby");
+        c.setSpeciesId(1);
+        c.setBackgroundId(3);
+        c.setAlignmentId(4);
+        c.setAppUserId(3);
+        assertFalse(service.addCharacter(c).isSuccess());
+    }
+
+    @Test
+    void shouldNotAddMissingBackground() {
+        Character c = new Character();
+        c.setCharacterName("Blobby");
+        c.setSpeciesId(1);
+        c.setClassId(2);
+        c.setAlignmentId(4);
+        c.setAppUserId(3);
+        assertFalse(service.addCharacter(c).isSuccess());
+    }
+
+    @Test
+    void shouldNotAddMissingAlignment() {
+        Character c = new Character();
+        c.setCharacterName("Blobby");
+        c.setSpeciesId(1);
+        c.setClassId(2);
+        c.setBackgroundId(3);
+        c.setAppUserId(3);
+        assertFalse(service.addCharacter(c).isSuccess());
+    }
+
+    @Test
+    void shouldNotAddMissingUser() {
+        Character c = new Character();
+        c.setCharacterName("Blobby");
+        c.setSpeciesId(1);
+        c.setClassId(2);
+        c.setBackgroundId(3);
+        c.setAlignmentId(4);
+        assertFalse(service.addCharacter(c).isSuccess());
+    }
+
+    @Test
+    void shouldUpdateValid() {
+        Character c = new Character(1, "Bobby B Brogubus", 1, 1, 1, 1,2);
+        assertTrue(service.updateCharacter(c).isSuccess());
+        assertEquals("Bobby B Brogubus", service.findCharacterById(1).getCharacterName());
+    }
+
+    @Test
+    void shouldNotUpdateInvalid() {
+        Character c = new Character(100, "Bobby B Brogubus", 1, 1, 1, 1,2);
+        assertFalse(service.updateCharacter(c).isSuccess());
+    }
+
+    @Test
+    void shouldDeleteExisting() {
+        int prevSize = service.findAllCharacters().size();
+        assertTrue(service.deleteCharacter(3));
+        assertEquals(prevSize - 1, service.findAllCharacters().size());
+    }
+
+    @Test
+    void shouldNotDeleteMissing() {
+        int prevSize = service.findAllCharacters().size();
+        assertFalse(service.deleteCharacter(3000));
+        assertEquals(prevSize, service.findAllCharacters().size());
     }
 }
