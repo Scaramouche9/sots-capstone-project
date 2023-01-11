65e7d0686d5c937ab1facf05946c2515c0c4ee37
mtaylordev <115902140+mtaylordev@users.noreply.github.com>
2023-01-11 10:38:44 -0600

    Revert "Minor bugfixing and suggestions from pre-demo implemented"

--- char-creator-client/src/Components/Character.js
+++ char-creator-client/src/Components/Character.js
@@ -1,15 +1,8 @@
-import React from "react";
-import { Link, useHistory } from "react-router-dom";
-import {Image} from 'cloudinary-react'
-
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Character(props) {
 	const history = useHistory();
-  
-  const handleDeleteButtonClick = () => {
-
-        props.setCharacterToConfirm(props.character);
-    }
 
 	
 	return (
@@ -19,17 +12,9 @@ export default function Character(props) {
 		(<tr id="character-table">
 			<th scope="row">{props.userCharacters.indexOf(props.character) + 1}</th>
 			<td className="character-list-name">{props.character.characterName}</td>
-      <td className="character-list-image">
-            {(props.character.image) &&
-            (<Image
-            style={{width: 200}}
-            cloudName='dr8dbzjws'
-            publicId={props.character.image}></Image>)}
-            </td>
 			<td className="character-list-description">
 				{props.character.description}
 			</td>
-
 
 			<td className="character-list-view-link">
 				<Link to={`/characters/view/${props.character.characterId}`}>
@@ -48,9 +33,13 @@ export default function Character(props) {
 				</Link>
 			</td>
 
-			<td className="character-list-delete-btn" ><button onClick={() => handleDeleteButtonClick()} >Delete</button></td>
+			<td
+				className="character-list-delete-btn"
+				onClick={() => props.deleteCharacter(props.character)}
+			>
+				<button>Delete</button>
+			</td>
 			
 		</tr>)
 	);
 }
-
