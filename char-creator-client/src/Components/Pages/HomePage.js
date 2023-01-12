6c98d54eb97a866f6f412a809115eb5c5a3bdc71
Matt Taylor <mtaylor@dev-10.com>
2023-01-12 12:37:56 -0600

    added scroll image to home page and made classNames for future use"

--- char-creator-client/src/Components/Pages/HomePage.js
+++ char-creator-client/src/Components/Pages/HomePage.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Image } from 'cloudinary-react';
 
 export default function HomePage() {
 	return (
-		<div>
+		<div className='home-page-container'>
 			<section>
-				<p className="home-p">
+			<Image
+				cloudName="dr8dbzjws"
+				publicId='https://res.cloudinary.com/dr8dbzjws/image/upload/v1673545654/d3euaz1kbzxa2t1s82ji.png'
+				></Image>
+				<p className="centered-home-page-text">
 					Please login or create an account to manage your characters.
 				</p>
+			
+				
 			</section>
 		</div>
 	);
